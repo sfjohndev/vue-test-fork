38fc0eb16fbdbb54be4322f73f57af5db124e839
[neimanis] <[ernestsneimanis]>
2023-04-03 02:27:08 +0200

    third build

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -11,8 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">What Are we doing</RouterLink>
+        <RouterLink to="/about">What is this</RouterLink>
       </nav>
     </div>
   </header>
